c7de14c40d42b2e3950f28e270a3b3f8fec12cce
Blake Dempsey <bdempsey864@gmail.com>
2021-06-11 16:11:10 -0600

    better next time

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,11 @@
 class Enigma
+  
   def initialize
+  end
+
+  def encrypt(encryption, key, date)
+  end
+
+  def decrypt(decryption, key, date)
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe Enigma do
   it 'exists' do
     expect(@enigma).to be_an(Enigma)
   end
+
+  it 'can encrypt a message' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({
+      encryption: "keder ohulw", 
+      key: "02715", 
+      date: "040895"
+    })
+  end
+
+  it 'can decrypt a message' do
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq.({
+      decryption: "hello world",
+      key: "02175",
+      date: "040895"
+    })
+  end
+
 end
